b420a9b706196419e89d95193a25b1b6bfae57c8
richJJackson <165043314+richJJackson@users.noreply.github.com>
2024-12-13 11:47:05 +0000

    submission

--- R/didiPlot.R
+++ R/didiPlot.R
@@ -1,13 +1,22 @@
-didiPlot <- function(st,cen,x){
-
-  ### estimating function
-  s_Est <- didiEst(st,cen,x,type="survival")
+#' Function to plot the Didi estimator.
+#'
+#' This function to plot the 'Didi' estimator.  This isuses the 'filled.contour'
+#' opint in base graphics and is now deprecited hving been replaced by
+#' plot.didi() which makes use of ggplot2.
+#"
+#' @param x an object of class "didi"
+#' @details This takes the inputs of the "didi" object and plots them using a
+#' filled contour plot.
+#' @returns A fileld contour plot for the didi estimator
+#' @import graphics
+#' @export
+didiPlot <- function(x){
 
   #### Plotting
-  zmat <-(s_Est$xMatrix)
-  n <- length(s_Est$cov)
-  time <- s_Est$time
-  cov <- s_Est$cov+1e-6
+  zmat <-(x$xMatrix)
+  n <- length(x$cov)
+  time <- x$time
+  cov <- x$cov+1e-6
   filled.contour(time,cov,zmat)
 
   }
